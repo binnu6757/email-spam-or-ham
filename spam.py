81f15ff93a45ea516afe2284c46a01a78fa6649f
binnu6757 <reddysab11@gmail.com>
2024-07-17 14:44:31 +0530

    spam.py

--- spam.py
+++ spam.py
@@ -6,12 +6,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 
 # streamlit app
-st.image(r"C:\Users\reddy\OneDrive\Desktop\new logo.jpg")
+st.image(r"new logo.jpg")
 st.title("EMAIL SPAM OR HAM CLASSIFIER")
 
 # Loading the trained model 
-model = pickle.load(open(r"C:\Users\reddy\machine learning\nb.pkl","rb"))
-model1 = pickle.load(open(r"C:\Users\reddy\machine learning\bow.pkl","rb"))
+model = pickle.load(open(r"nb.pkl","rb"))
+model1 = pickle.load(open(r"bow.pkl","rb"))
 
 # input text box
 email =st.text_area("Please Enter Your Email Text To Classify")
@@ -22,11 +22,11 @@ prediction = model.predict(checking)[0]
 if st.button("Predict"):
     if prediction == "spam":
         st.write("This email is SPAM")
-        st.image(r"C:\Users\reddy\OneDrive\Desktop\new spam logo.jpg")
+        st.image(r"new spam logo.jpg")
 
     else:
         st.write("This Not A Spam")
-        st.image(r"C:\Users\reddy\OneDrive\Desktop\new thumb.jpg")
+        st.image(r"new thumb.jpg")
         
 
 
